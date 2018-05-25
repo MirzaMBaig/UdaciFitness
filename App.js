c9d99289b3377cf9bba9ba580acd716b857a1fa7
mirza.baig <mirza.baig@dwp.gsi.gov.uk>
2018-05-26 00:26:14 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View } from 'react-native';
+import {AddEntry} from './component/AddEntry'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View >
+        <AddEntry/>
+      </View>
+    );
+  }
+}
---
+++ app/component/AddEntry.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>test</Text>
+            </View>
+        )
+    };
+}
